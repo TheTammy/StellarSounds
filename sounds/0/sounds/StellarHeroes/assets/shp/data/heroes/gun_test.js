03e59e23e2b7803f21ca32eb466ebb546f608fe1
shadow00dev <45771918+shadow00dev@users.noreply.github.com>
2023-05-06 19:35:27 +0100

    made a mistake adding the heropack here

--- sounds/0/sounds/StellarHeroes/assets/shp/data/heroes/gun_test.js
+++
@@ -1,6 +0,0 @@
-function init(hero) {
-    hero.setName("Test");
-    hero.setTier(3);
-
-    hero.setChestplate("Suit");
-}
--- sounds/0/sounds/StellarHeroes/assets/shp/renderers/heroes/spider_man_unlimited_shaded.js
+++
@@ -1,6 +0,0 @@
-extend("shp:spider_man_unlimited");
-loadTextures({
-    "base": "shp:spider_man_unlimited_shaded",
-    "suit": "shp:spider_man_unlimited_shaded_suit.tx.json",
-    "mask": "shp:spider_man_unlimited_mask_shaded.tx.json"
-});
